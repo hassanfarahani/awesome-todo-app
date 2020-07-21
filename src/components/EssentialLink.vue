b72fd2a6e129668de1a1936c870487ca3d8a59f2
Hassan Farahani <farahani.h1360@gmail.com>
2020-07-21 09:55:15 -0700

    first commit

---
+++ src/components/EssentialLink.vue
@@ -0,0 +1,40 @@
+<template>
+  <q-item
+    clickable
+    :to="route"
+    exact
+  >
+    <q-item-section
+      v-if="icon"
+      avatar
+    >
+      <q-icon :name="icon" />
+    </q-item-section>
+
+    <q-item-section>
+      <q-item-label>{{ title }}</q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
+
+<script>
+export default {
+  name: 'EssentialLink',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+
+    icon: {
+      type: String,
+      default: ''
+    },
+
+    route: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,90 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title class="absolute-center">
+          Awesome Todo
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-footer>
+      <q-tabs>
+        <q-route-tab v-for="nav in navs" :key="nav.label" :icon="nav.icon" :label="nav.title" :to="nav.route" />
+      </q-tabs>
+    </q-footer>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      :breakpoint="767"
+      :width="250"
+      show-if-above
+      bordered
+      content-class="bg-primary"
+    >
+      <q-list dark>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          Navigation
+        </q-item-label>
+        <EssentialLink
+          v-for="nav in navs"
+          :key="nav.label"
+          v-bind="nav"
+          class="text-grey-5"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+export default {
+  name: 'layout',
+
+  components: {
+    EssentialLink
+  },
+
+  data () {
+    return {
+      leftDrawerOpen: false,
+      navs: [
+        {
+          title: 'Todo',
+          icon: 'list',
+          route: '/'
+        },
+        {
+          title: 'Settings',
+          icon: 'settings',
+          route: '/settings'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  @media screen and (min-width: 768px) {
+    .q-footer {
+      display: none;
+    }
+  }
+
+  .q-drawer {
+    .q-router-link--exact-active {
+      color: #fff !important;
+    }
+  }
+</style>
+
